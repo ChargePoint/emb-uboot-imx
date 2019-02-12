84edba7a09c337c2ac70c0556126aa271cf252e0
vgahlan-cpi <vikas.gahlan@chargepoint.com>
2019-02-12 15:55:12 -0800

    ChargePoint PBC bootloader changes upgrade to imx-4.19.35_1.1.0 release
    Update build script
    
    EMBSW-7008: PBC: uboot board layer for PBC controller (#2)
    
    * EMBSW-7008: PBC: uboot board layer for PBC controller
    
    First cut at u-boot for PBC (yet untested for want of HW).
    
    * remove video
    
    * clean up some build warnings
    
    * Niranjan suggestions, more complete GPIO mappings, new NXP PinTool-generated file from Byron's latest .mex file.
    
    * more fixes- USB is now working (in my hacked tests on cpnk anyway)
    
    * more Niranjan suggestions
    
    * fix CONFIG_PHY_MARVELL typo
    
    * [EMBSW-7011] PBC: DDR3L memory calibration and code into JTAG and uboot
    
    * fix the CPU is a MX6D...  NOT a MX6DL
    
    * add missing CONFIG_SYS_MMC_IMG_LOAD_PART define to 5
    
    * DCD update, set MMDC reg 0x021b0400 to value 0x514201f0, fixes usb otg reset/hang issue
    
    * define CONFIG_MMCROOT "/dev/mmcblk0p5"
    
    * fix commit 21ff9c286d, where dcd value needed to be 0x14420000
    
    * add setting of mmcrootdev and mmcrootpart
    
    * call our local board_late_mmc_env_init()
    
    * u-boot take IO expander out of reset upon boot
    
    * Set RGMII I/O drive strength to 1P5V
    
    [EMBSW-7229] Add board_type pbc in env for init system (#3)
    
    PBC Rev2 memory configuration changes
    
    changes to pin muxing for pbc rev 2
    
    .gitignore and builduboot_imx8 script

---
+++ builduboot_imx8.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+source ../toolchain/environment-setup-aarch64-poky-linux
+cores=`cat /proc/cpuinfo | grep processor | wc -l`
+threads=`expr $cores + 2`
+export ARCH=arm64
+mkdir -p build
+make O=build mrproper
+make O=build distclean
+make O=build clean
+make O=build imx8qxp_mek_defconfig
+#make O=build ARCH=arm mx6dlsabresd_pbc_defconfig
+make O=build -j $threads
