09c5bc8ea7321c779cac80cca3edea95ab98d5e2
Charles Hardin <charles.hardin@chargepoint.com>
2022-01-12 10:43:23 -0800

    board: perform the rename of the chargepoint name from sbc -> mcb
    This is intended to be the no functional change that just switches
    the names of the files to match what is expected to match the board
    targets.
    
    Signed-off-by: Charles Hardin <charles.hardin@chargepoint.com>

--- board/chargepoint/imx8mm_mcb/imx8mm_mcb.c
+++ board/chargepoint/imx8mm_mcb/imx8mm_mcb.c
@@ -54,7 +54,7 @@ int board_late_init(void)
 #endif
 
 #ifdef CONFIG_ENV_VARS_UBOOT_RUNTIME_CONFIG
-	env_set("board_name", "SBC");
+	env_set("board_name", "MCB");
 	env_set("board_rev", "iMX8MM");
 #endif
 	return 0;
