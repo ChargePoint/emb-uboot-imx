f75f4fec70239a78acc6c082972cba9ffaa87c87
Aswath Gajendran <aswath.gajendran@chargepoint.com>
2022-02-02 10:55:10 -0800

    board-kestrel: add imx8dxl_kcb support
    Add support for Kestrel KCB board. Bring up
    done with imx8dxl evk board. Default device
    tree is used as of now.
    
    split kcb config into secure and normal boot
    
    export serial numder cmdline.Pass the unique id
    of the cpu as the serial number in the device
    tree.
    
    PLAT-4325 - https://chargepoint.atlassian.net/browse/PLAT-4325
    
    Signed-off-by: Aswath Gajendran <aswath.gajendran@chargepoint.com>

---
+++ include/configs/chargepoint_imx8dxl_kcb.h
@@ -0,0 +1,305 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Copyright 2020 NXP
+ */
+
+#ifndef __IMX8DXL_EVK_H
+#define __IMX8DXL_EVK_H
+
+#include <linux/sizes.h>
+#include <asm/arch/imx-regs.h>
+
+#include "imx_env.h"
+
+#ifdef CONFIG_SPL_BUILD
+#define CONFIG_SPL_MAX_SIZE				(128 * 1024)
+#define CONFIG_SYS_MONITOR_LEN				(1024 * 1024)
+#define CONFIG_SYS_MMCSD_RAW_MODE_U_BOOT_USE_SECTOR
+#define CONFIG_SYS_MMCSD_RAW_MODE_U_BOOT_SECTOR		0x1040 /* (32K + 2Mb)/sector_size */
+
+/*
+ * 0x08081000 - 0x08180FFF is for m4_0 xip image,
+  * So 3rd container image may start from 0x8181000
+ */
+#define CONFIG_SYS_UBOOT_BASE 0x08181000
+
+#define CONFIG_SYS_NAND_U_BOOT_OFFS     (0x8000000)  /*Put the FIT out of first 128MB boot area */
+#define CONFIG_SPL_NAND_BASE
+#define CONFIG_SPL_NAND_IDENT
+
+#define CONFIG_SYS_MMCSD_FS_BOOT_PARTITION		0
+
+#define CONFIG_SPL_LDSCRIPT		"arch/arm/cpu/armv8/u-boot-spl.lds"
+/*
+ * The memory layout on stack:  DATA section save + gd + early malloc
+ * the idea is re-use the early malloc (CONFIG_SYS_MALLOC_F_LEN) with
+ * CONFIG_SYS_SPL_MALLOC_START
+ */
+#define CONFIG_SPL_STACK		0x822ffff0
+#define CONFIG_SPL_BSS_START_ADDR      0x82280000
+#define CONFIG_SPL_BSS_MAX_SIZE		0x1000	/* 4 KB */
+#define CONFIG_SYS_SPL_MALLOC_START	0x82200000
+#define CONFIG_SYS_SPL_MALLOC_SIZE     0x80000	/* 512 KB */
+#define CONFIG_SERIAL_LPUART_BASE	0x5a060000
+#define CONFIG_MALLOC_F_ADDR		0x82200000
+
+#define CONFIG_SPL_RAW_IMAGE_ARM_TRUSTED_FIRMWARE
+
+#define CONFIG_SPL_ABORT_ON_RAW_IMAGE
+
+#endif
+
+#define CONFIG_REMAKE_ELF
+
+#define CONFIG_BOARD_EARLY_INIT_F
+
+#define CONFIG_CMD_READ
+
+
+#undef CONFIG_CMD_EXPORTENV
+#undef CONFIG_CMD_IMPORTENV
+#undef CONFIG_CMD_IMLS
+
+#undef CONFIG_CMD_CRC32
+
+#define CONFIG_SYS_FSL_ESDHC_ADDR       0
+#define USDHC1_BASE_ADDR                0x5B010000
+#define USDHC2_BASE_ADDR                0x5B020000
+
+#define CONFIG_ENV_OVERWRITE
+
+
+#define CONFIG_PCIE_IMX
+#define CONFIG_CMD_PCI
+#define CONFIG_PCI_SCAN_SHOW
+
+#define CONFIG_ENV_VARS_UBOOT_RUNTIME_CONFIG
+
+/* Link Definitions */
+#define CONFIG_LOADADDR			0xA8000000
+
+#define CONFIG_SYS_LOAD_ADDR           CONFIG_LOADADDR
+
+#define CONFIG_SYS_INIT_SP_ADDR         0x80200000
+
+#ifdef CONFIG_QSPI_BOOT
+#define CONFIG_ENV_SECT_SIZE	(128 * 1024)
+#define CONFIG_ENV_SPI_BUS	CONFIG_SF_DEFAULT_BUS
+#define CONFIG_ENV_SPI_CS	CONFIG_SF_DEFAULT_CS
+#define CONFIG_ENV_SPI_MODE	CONFIG_SF_DEFAULT_MODE
+#define CONFIG_ENV_SPI_MAX_HZ	CONFIG_SF_DEFAULT_SPEED
+#else
+#define CONFIG_SYS_MMC_ENV_PART		0	/* user area */
+#endif
+
+#define CONFIG_SYS_MMC_IMG_LOAD_PART	2
+
+/* USDHC2 for SD */
+#define CONFIG_SYS_MMC_ENV_DEV		1   /* USDHC2 */
+#define CONFIG_MMCROOT			"/dev/mmcblk1p2"  /* USDHC2 */
+#define CONFIG_SYS_FSL_USDHC_NUM	2
+
+/* Size of malloc() pool */
+#define CONFIG_SYS_MALLOC_LEN           SZ_32M
+#define CONFIG_SYS_BOOTM_LEN            (64 << 24)
+#define CONFIG_SYS_XIMG_LEN             CONFIG_SYS_BOOTM_LEN
+
+#define CONFIG_SYS_SDRAM_BASE		0x80000000
+#define PHYS_SDRAM_1			0x80000000
+#define PHYS_SDRAM_2			0x880000000
+
+/* total DDR is 1GB */
+#if defined(CONFIG_TARGET_IMX8DXL_DDR3_EVK)
+#define PHYS_SDRAM_1_SIZE		0x20000000
+#else
+#define PHYS_SDRAM_1_SIZE		0x40000000	/* 1 GB */
+#endif
+
+#define PHYS_SDRAM_2_SIZE		0x00000000
+
+#define CONFIG_SYS_MEMTEST_START    0xA0000000
+#define CONFIG_SYS_MEMTEST_END      (CONFIG_SYS_MEMTEST_START + (PHYS_SDRAM_1_SIZE >> 2))
+
+/* Serial */
+#define CONFIG_BAUDRATE			115200
+
+/* Monitor Command Prompt */
+#define CONFIG_SYS_PROMPT_HUSH_PS2     "> "
+#define CONFIG_SYS_CBSIZE              2048
+#define CONFIG_SYS_MAXARGS             64
+#define CONFIG_SYS_BARGSIZE CONFIG_SYS_CBSIZE
+#define CONFIG_SYS_PBSIZE		(CONFIG_SYS_CBSIZE + \
+					sizeof(CONFIG_SYS_PROMPT) + 16)
+
+/* Generic Timer Definitions */
+#define COUNTER_FREQUENCY		8000000	/* 8MHz */
+
+#ifndef CONFIG_DM_PCA953X
+#define CONFIG_PCA953X
+#define CONFIG_CMD_PCA953X
+#define CONFIG_CMD_PCA953X_INFO
+#endif
+
+/* MT35XU512ABA1G12 has only one Die, so QSPI0 B won't work */
+#ifdef CONFIG_FSL_FSPI
+#define FSL_FSPI_FLASH_SIZE		SZ_64M
+#define FSL_FSPI_FLASH_NUM		1
+#define FSPI0_BASE_ADDR			0x5d120000
+#define FSPI0_AMBA_BASE			0
+#endif
+
+#define CONFIG_SERIAL_TAG
+
+#ifdef CONFIG_NAND_MXS
+#define CONFIG_CMD_NAND_TRIMFFS
+
+/* NAND stuff */
+#define CONFIG_SYS_MAX_NAND_DEVICE     1
+#define CONFIG_SYS_NAND_BASE           0x40000000
+#define CONFIG_SYS_NAND_5_ADDR_CYCLE
+#define CONFIG_SYS_NAND_ONFI_DETECTION
+#define CONFIG_SYS_NAND_USE_FLASH_BBT
+
+#endif
+
+/* USB Config */
+#define CONFIG_USBD_HS
+
+#define CONFIG_USB_MAX_CONTROLLER_COUNT 2
+
+/* USB OTG controller configs */
+#ifdef CONFIG_USB_EHCI_HCD
+#define CONFIG_USB_HOST_ETHER
+#define CONFIG_USB_ETHER_ASIX
+#define CONFIG_MXC_USB_PORTSC		(PORT_PTS_UTMI | PORT_PTS_PTW)
+#endif
+
+/* Networking */
+#define CONFIG_FEC_XCV_TYPE		RGMII
+#define FEC_QUIRK_ENET_MAC
+#define CONFIG_FEC_MXC_PHYADDR          0x1
+
+#define DWC_NET_PHYADDR			0
+#ifdef CONFIG_DWC_ETH_QOS
+#define CONFIG_SYS_NONCACHED_MEMORY     (1 * SZ_1M)     /* 1M */
+#endif
+#define CONFIG_ETHPRIME                 "eth1"
+#define PHY_ANEG_TIMEOUT 20000
+
+#if defined(CONFIG_DM_VIDEO)
+#define CONFIG_VIDEO_MXS
+#define CONFIG_VIDEO_LINK
+#define CONFIG_VIDEO_LOGO
+#define CONFIG_SPLASH_SCREEN
+#define CONFIG_SPLASH_SCREEN_ALIGN
+#define CONFIG_CMD_BMP
+#define CONFIG_BMP_16BPP
+#define CONFIG_BMP_24BPP
+#define CONFIG_BMP_32BPP
+#define CONFIG_VIDEO_BMP_RLE8
+#define CONFIG_VIDEO_BMP_LOGO
+#endif
+
+/* Serial */
+#define CONSOLE_DEV	"ttyLP0"
+
+#define CONFIG_MFG_ENV_SETTINGS \
+	"mfgtool_args=setenv bootargs console=${console},${baudrate} " \
+		"reboot=h earlyprintk=serial ignore_loglevel debug " \
+		"clk_ignore_unused root=/dev/ram " \
+		"\0" \
+	"bootcmd_mfg=run mfgtool_args; " \
+		"echo \"Run fastboot ...\"; fastboot 0; "  \
+		"\0" \
+
+
+#define CONFIG_EXTRA_ENV_SETTINGS \
+	CONFIG_MFG_ENV_SETTINGS \
+	"console=" CONSOLE_DEV "\0" \
+	"initrd_high=0xffffffffffffffff\0" \
+	"bootenvpart=0:1\0" \
+	"bootenv=uboot.env\0" \
+	"bootfile=fitImage\0" \
+	"bootparta=0:2\0" \
+	"bootpartb=0:3\0" \
+	"importbootenv=echo Importing env from mmc ${bootenvpart} ...; " \
+		"part uuid mmc ${bootenvpart} bootenvuuid; " \
+		"if ext4load mmc ${bootenvpart} " \
+			"${loadaddr} ${bootenv}; then " \
+				"env import -c ${loadaddr} ${filesize} " \
+					"display fitconfig " \
+					"trybootpart bootpart bootlabel; " \
+		"elif ext4load mmc ${bootenvpart} " \
+			"${loadaddr} ${bootenv}-backup; then " \
+				"env import -c ${loadaddr} ${filesize} " \
+					"display fitconfig " \
+					"bootpart bootlabel; " \
+				"env export -c ${loadaddr} " \
+					"display fitconfig " \
+					"trybootpart bootpart bootlabel; " \
+				"ext4write mmc ${bootenvpart} ${loadaddr} " \
+					"/${bootenv} ${filesize}; " \
+		"fi; " \
+	"\0" \
+	"mmctryboot=" \
+		"if test -n ${trybootpart}; then " \
+			"echo Try-boot ${bootfile} from " \
+				"mmc ${trybootpart} ...; " \
+			"setenv -f _trybootpart ${trybootpart}; " \
+			"setenv -f trybootpart; " \
+			"env export -c ${loadaddr} " \
+				"display fitconfig " \
+				"trybootpart bootpart bootlabel; " \
+			"ext4write mmc ${bootenvpart} ${loadaddr} " \
+				"/${bootenv} ${filesize}; " \
+			"part uuid mmc ${_trybootpart} bootuuid; " \
+			"setenv bootargs ${bootargs_secureboot} " \
+				"console=${console} " \
+				"bootenv=PARTUUID=${bootenvuuid} " \
+				"root=PARTUUID=${bootuuid} rootwait rw; " \
+			"echo Try-booting ${bootfile} from mmc " \
+				"${_trybootpart} ...; " \
+			"ext4load mmc ${_trybootpart} ${loadaddr} " \
+				"${bootfile} && " \
+					"bootm ${bootmarg}; " \
+		"fi; " \
+	"\0" \
+	"mmcboot=setenv -f _bootpart ${bootparta}; " \
+		"run importbootenv; " \
+		"setenv -f bootmarg ${loadaddr}; " \
+		"if test -n ${fitconfig}; then " \
+			"setenv -f bootmarg ${loadaddr}#${fitconfig}; " \
+		"fi; " \
+		"run mmctryboot; " \
+		"if test -n ${bootpart} && test ${bootpart} != none; then " \
+			"setenv -f _bootpart ${bootpart}; " \
+		"fi; " \
+		"echo Booting ${bootfile} from mmc ${_bootpart} ...; " \
+		"part uuid mmc ${_bootpart} bootuuid; " \
+		"setenv bootargs ${bootargs_secureboot} console=${console} " \
+			"bootenv=PARTUUID=${bootenvuuid} " \
+			"root=PARTUUID=${bootuuid} rootwait rw; " \
+		"ext4load mmc ${_bootpart} ${loadaddr} ${bootfile} && " \
+			"bootm ${bootmarg}; " \
+		"if test ${_bootpart} = ${bootparta}; then " \
+			"setenv -f _bootpart ${bootpartb}; " \
+		"else " \
+			"setenv -f _bootpart ${bootparta}; " \
+		"fi; " \
+		"echo Failover boot ${bootfile} from mmc ${_bootpart} ...; " \
+		"part uuid mmc ${_bootpart} bootuuid; " \
+		"setenv bootargs ${bootargs_secureboot} console=${console} " \
+			"bootenv=PARTUUID=${bootenvuuid} " \
+			"root=PARTUUID=${bootuuid} rootwait rw; " \
+		"ext4load mmc ${_bootpart} ${loadaddr} ${bootfile} && " \
+			"bootm ${bootmarg}; " \
+	"\0"
+
+
+#define CONFIG_BOOTCOMMAND \
+	"run mmcboot"
+
+#define CONFIG_BOOTARGS \
+	"console=" CONSOLE_DEV ",115200 earlyprintk=serial ignore_loglevel"
+
+#endif /* __IMX8DXL_EVK_H */
