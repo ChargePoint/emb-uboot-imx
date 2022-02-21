5b312bf8639bed16cf584db1d20ad348550e332d
Charles Hardin <charles.hardin@chargepoint.com>
2022-02-21 09:40:30 -0800

    board/mcb: export the serial number, secureboot, and etheraddr
    The data transfer to the kernel is thru the device tree fixups
    and the bootargs. So, enable the expectent environment changes:
    
    - when hab is closed pass a uboot-secureboot for bootargs
    - pass the unique id of the cpu as the serial-number in the devicetree
    - pass an ethernet address that is burnt into the fuses or derived
      from the unique id of the cpu
    
    Signed-off-by: Charles Hardin <charles.hardin@chargepoint.com>

--- board/chargepoint/imx8mm_mcb/imx8mm_mcb.c
+++ board/chargepoint/imx8mm_mcb/imx8mm_mcb.c
@@ -57,5 +57,46 @@ int board_late_init(void)
 	env_set("board_name", "MCB");
 	env_set("board_rev", "iMX8MM");
 #endif
+
+#if defined(CONIG_NOT_UUU_BUILD)
+	/* set an environment that this is a secure boot */
+	if (imx_hab_is_enabled()) {
+		env_set("bootargs_secureboot", "uboot-secureboot");
+	}
+#endif
+
 	return 0;
 }
+
+#ifdef CONFIG_OF_BOARD_SETUP
+int ft_board_setup(void *blob, bd_t *bd)
+{
+	uint32_t uid_high, uid_low;
+
+	/*
+	 * i.MX8M Configuration and Manufacturing Info
+	 *   OCOTP_CFG0: OTP Bank 0, word 1
+	 *   OCOTP_CFG1: OTP Bank 0, word 2
+	 */
+	do {
+		struct ocotp_regs *ocotp =
+			(struct ocotp_regs *)OCOTP_BASE_ADDR;
+		struct fuse_bank *bank = &ocotp->bank[0];
+		struct fuse_bank0_regs *fuse =
+			(struct fuse_bank0_regs *)bank->fuse_regs;
+
+		uid_high = readl(&fuse->uid_high);
+		uid_low = readl(&fuse->uid_low);
+	} while(0);
+
+	if (!env_get("serial#")) {
+		char serial_str[17];
+
+		snprintf(serial_str, sizeof(serial_str),
+			 "%08x%08x", uid_high, uid_low);
+		env_set("serial#", serial_str);
+
+		fdt_root(blob);
+	}
+}
+#endif
