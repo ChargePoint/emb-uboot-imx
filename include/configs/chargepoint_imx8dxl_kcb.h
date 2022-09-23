854d0c09a62cac918c29ca9873d25568a2a3b2d2
Charles Hardin <charles.hardin@chargepoint.com>
2022-09-23 13:40:05 -0700

    boards: add a systemd.unit for the rescue target on failover boots
    When the primary partition does not boot, add the rescue target
    into the bootargs to force systemd to run a rescue mode. This can
    be tested by delete the kernel of the committed partition and
    triggering the failover boot mode.
    
    As watchdogs and other commits are added the failover case will
    be triggered under multiple conditions but the outcome from the
    boot will fall into the rescue target being run.
    
    Fixes: PLAT-6388
    
    Signed-off-by: Charles Hardin <charles.hardin@chargepoint.com>

--- include/configs/chargepoint_imx8dxl_kcb.h
+++ include/configs/chargepoint_imx8dxl_kcb.h
@@ -252,7 +252,8 @@
 			"setenv bootargs ${bootargs_secureboot} " \
 				"console=${console} " \
 				"bootenv=PARTUUID=${bootenvuuid} " \
-				"root=PARTUUID=${bootuuid} rootwait rw; " \
+				"root=PARTUUID=${bootuuid} rootwait rw " \
+				"${bootargs_append}; " \
 			"echo Try-booting ${bootfile} from mmc " \
 				"${_trybootpart} ...; " \
 			"ext4load mmc ${_trybootpart} ${loadaddr} " \
@@ -275,7 +276,8 @@
 		"part uuid mmc ${_bootpart} bootuuid; " \
 		"setenv bootargs ${bootargs_secureboot} console=${console} " \
 			"bootenv=PARTUUID=${bootenvuuid} " \
-			"root=PARTUUID=${bootuuid} rootwait rw; " \
+			"root=PARTUUID=${bootuuid} rootwait rw " \
+			"${bootargs_append}; " \
 		"ext4load mmc ${_bootpart} ${loadaddr} ${bootfile} && " \
 			"bootm ${bootmarg}; " \
 		"if test ${_bootpart} = ${bootparta}; then " \
@@ -287,7 +289,9 @@
 		"part uuid mmc ${_bootpart} bootuuid; " \
 		"setenv bootargs ${bootargs_secureboot} console=${console} " \
 			"bootenv=PARTUUID=${bootenvuuid} " \
-			"root=PARTUUID=${bootuuid} rootwait rw; " \
+			"root=PARTUUID=${bootuuid} rootwait rw " \
+			"systemd.unit=rescue.target " \
+			"${bootargs_append}; " \
 		"ext4load mmc ${_bootpart} ${loadaddr} ${bootfile} && " \
 			"bootm ${bootmarg}; " \
 	"\0"
