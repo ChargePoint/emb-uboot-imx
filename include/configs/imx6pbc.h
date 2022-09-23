6f95a136672cd79c235baa7a154ea3737e0acd21
Charles Hardin <charles.hardin@chargepoint.com>
2022-09-23 13:17:40 -0700

    boards: add a systemd.unit for the rescue target on failover boots
    When the primary partition does not boot, add the rescue target
    into the bootargs to force systemd to run a rescue mode. This can
    be tested by delete the kernel of the committed partition and
    triggering the failover boot mode.
    
    As watchdogs and other commits are added the failover case will
    be triggered under multiple conditions but the outcome from the
    boot will fall into the rescue target being run.
    
    Fixes: PLAT-6388
    
    Signed-off-by: Charles Hardin <charles.hardin@chargepoint.com>
    (cherry picked from commit 4363239e0af3fd60429b9c5bd2f123421cda1485)

--- include/configs/imx6pbc.h
+++ include/configs/imx6pbc.h
@@ -264,6 +264,7 @@
 		"setenv bootargs ${bootargs_secureboot} console=${console} " \
 			"bootenv=PARTUUID=${bootenvuuid} " \
 			"root=PARTUUID=${bootuuid} rootwait rw " \
+			"systemd.unit=rescue.target " \
 			"${bootargs_append}; " \
 		"ext4load mmc ${_bootpart} ${loadaddr} ${bootfile} && " \
 			"bootm ${loadaddr}; " \
