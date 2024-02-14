24964ec7bbef57a555e865e338b5a49ae3165e30
Geert Lens <geert.lens@chargepoint.com>
2024-02-14 15:12:39 +0100

    fix(cpcm): make config more consistent
    - Uses the same boot logic as kcb, ucb and mcb.
    - Allows USB boot to work properly.
    - Removed Matter specific config
    
    Signed-off-by: Geert Lens <geert.lens@chargepoint.com>

--- include/configs/chargepoint_imx93_cpcm.h
+++ include/configs/chargepoint_imx93_cpcm.h
@@ -14,140 +14,107 @@
 #define CFG_SYS_UBOOT_BASE	\
 	(QSPI0_AMBA_BASE + CONFIG_SYS_MMCSD_RAW_MODE_U_BOOT_SECTOR * 512)
 
-#ifdef CONFIG_AHAB_BOOT
-#define AHAB_ENV "sec_boot=yes\0"
-#else
-#define AHAB_ENV "sec_boot=no\0"
-#endif
+#define PHY_ANEG_TIMEOUT 20000
 
-#ifdef CONFIG_DISTRO_DEFAULTS
-#define BOOT_TARGET_DEVICES(func) \
-	func(MMC, mmc, 0) \
-	func(MMC, mmc, 1) \
-	func(USB, usb, 0)
-
-#include <config_distro_bootcmd.h>
-#else
 #define BOOTENV
-#endif
 
-#define JH_ROOT_DTB    "imx93-chargepoint-cpcm-root.dtb"
-
-#define JAILHOUSE_ENV \
-	"jh_root_dtb=" JH_ROOT_DTB "\0" \
-	"jh_mmcboot=setenv fdtfile ${jh_root_dtb}; " \
-		    "setenv jh_clk clk_ignore_unused mem=1248MB kvm-arm.mode=nvhe; " \
-		    "if run loadimage; then run mmcboot;" \
-		    "else run jh_netboot; fi; \0" \
-	"jh_netboot=setenv fdtfile ${jh_root_dtb}; " \
-		    "setenv jh_clk clk_ignore_unused mem=1248MB kvm-arm.mode=nvhe; run netboot; \0 "
+#define CONSOLE_DEV	"ttyLP0"
 
 #define CFG_MFG_ENV_SETTINGS \
-	CFG_MFG_ENV_SETTINGS_DEFAULT \
-	"initrd_addr=0x83800000\0" \
-	"initrd_high=0xffffffffffffffff\0" \
-	"emmc_dev=0\0"\
-	"sd_dev=1\0" \
+	"console=" CONSOLE_DEV "\0" \
+	"mfgtool_args=setenv bootargs console=${console},${baudrate} " \
+		"reboot=h earlycon ignore_loglevel debug " \
+		"clk_ignore_unused root=/dev/ram " \
+		"\0" \
+	"bootcmd_mfg=run mfgtool_args; " \
+		"echo \"Run fastboot ...\"; fastboot 0; "  \
+		"\0" \
 
 /* Initial environment variables */
 #define CFG_EXTRA_ENV_SETTINGS		\
-	JAILHOUSE_ENV \
 	CFG_MFG_ENV_SETTINGS \
-	BOOTENV \
-	AHAB_ENV \
-	"prepare_mcore=setenv mcore_clk clk-imx93.mcore_booted;\0" \
-	"scriptaddr=0x83500000\0" \
-	"kernel_addr_r=" __stringify(CONFIG_SYS_LOAD_ADDR) "\0" \
-	"image=Image\0" \
-	"splashimage=0x90000000\0" \
-	"console=ttyLP0,115200 earlycon\0" \
-	"fdt_addr_r=0x83000000\0"			\
-	"fdt_addr=0x83000000\0"			\
-	"fdt_high=0xffffffffffffffff\0"		\
-	"cntr_addr=0x98000000\0"			\
-	"cntr_file=os_cntr_signed.bin\0" \
-	"boot_fit=no\0" \
-	"fdtfile=" CONFIG_DEFAULT_FDT_FILE "\0" \
-	"bootm_size=0x10000000\0" \
-	"mmcdev=" __stringify(CONFIG_SYS_MMC_ENV_DEV)"\0" \
-	"mmcpart=1\0" \
-	"mmcroot=/dev/mmcblk1p2 rootwait rw\0" \
-	"mmcautodetect=yes\0" \
-	"mmcargs=setenv bootargs ${jh_clk} ${mcore_clk} console=${console} root=${mmcroot}\0 " \
-	"loadbootscript=fatload mmc ${mmcdev}:${mmcpart} ${loadaddr} ${script};\0" \
-	"bootscript=echo Running bootscript from mmc ...; " \
-		"source\0" \
-	"loadimage=fatload mmc ${mmcdev}:${mmcpart} ${loadaddr} ${image}\0" \
-	"loadfdt=fatload mmc ${mmcdev}:${mmcpart} ${fdt_addr_r} ${fdtfile}\0" \
-	"loadcntr=fatload mmc ${mmcdev}:${mmcpart} ${cntr_addr} ${cntr_file}\0" \
-	"auth_os=auth_cntr ${cntr_addr}\0" \
-	"boot_os=booti ${loadaddr} - ${fdt_addr_r};\0" \
-	"mmcboot=echo Booting from mmc ...; " \
-		"run mmcargs; " \
-		"if test ${sec_boot} = yes; then " \
-			"if run auth_os; then " \
-				"run boot_os; " \
-			"else " \
-				"echo ERR: failed to authenticate; " \
-			"fi; " \
-		"else " \
-			"if test ${boot_fit} = yes || test ${boot_fit} = try; then " \
-				"bootm ${loadaddr}; " \
-			"else " \
-				"if run loadfdt; then " \
-					"run boot_os; " \
-				"else " \
-					"echo WARN: Cannot load the DT; " \
-				"fi; " \
-			"fi;" \
-		"fi;\0" \
-	"netargs=setenv bootargs ${jh_clk} ${mcore_clk} console=${console} " \
-		"root=/dev/nfs " \
-		"ip=dhcp nfsroot=${serverip}:${nfsroot},v3,tcp\0" \
-	"netboot=echo Booting from net ...; " \
-		"run netargs;  " \
-		"if test ${ip_dyn} = yes; then " \
-			"setenv get_cmd dhcp; " \
-		"else " \
-			"setenv get_cmd tftp; " \
+	"bootenvpart=2:1\0" \
+	"bootenv=uboot.env\0" \
+	"bootfile=fitImage\0" \
+	"bootparta=2:2\0" \
+	"bootpartb=2:3\0" \
+	"importbootenv=echo Importing env from mmc ${bootenvpart} ...; " \
+		"part uuid mmc ${bootenvpart} bootenvuuid; " \
+		"if ext4load mmc ${bootenvpart} " \
+			"${loadaddr} ${bootenv}; then " \
+				"env import -c ${loadaddr} ${filesize} " \
+					"display fitconfig " \
+					"trybootpart bootpart bootlabel; " \
+		"elif ext4load mmc ${bootenvpart} " \
+			"${loadaddr} ${bootenv}-backup; then " \
+				"env import -c ${loadaddr} ${filesize} " \
+					"display fitconfig " \
+					"bootpart bootlabel; " \
+				"env export -c ${loadaddr} " \
+					"display fitconfig " \
+					"trybootpart bootpart bootlabel; " \
+				"ext4write mmc ${bootenvpart} ${loadaddr} " \
+					"/${bootenv} ${filesize}; " \
 		"fi; " \
-		"if test ${sec_boot} = yes; then " \
-			"${get_cmd} ${cntr_addr} ${cntr_file}; " \
-			"if run auth_os; then " \
-				"run boot_os; " \
-			"else " \
-				"echo ERR: failed to authenticate; " \
-			"fi; " \
+	"\0" \
+	"mmctryboot=" \
+		"if test -n ${trybootpart}; then " \
+			"echo Try-boot ${bootfile} from " \
+				"mmc ${trybootpart} ...; " \
+			"setenv -f _trybootpart ${trybootpart}; " \
+			"setenv -f trybootpart; " \
+			"env export -c ${loadaddr} " \
+				"display fitconfig " \
+				"trybootpart bootpart bootlabel; " \
+			"ext4write mmc ${bootenvpart} ${loadaddr} " \
+				"/${bootenv} ${filesize}; " \
+			"part uuid mmc ${_trybootpart} bootuuid; " \
+			"setenv bootargs ${bootargs_secureboot} " \
+				"console=${console},${baudrate} " \
+				"bootenv=PARTUUID=${bootenvuuid} " \
+				"root=PARTUUID=${bootuuid} rootwait rw " \
+				"${bootargs_append}; " \
+			"echo Try-booting ${bootfile} from mmc " \
+				"${_trybootpart} ...; " \
+			"ext4load mmc ${_trybootpart} ${loadaddr} " \
+				"${bootfile} && " \
+					"bootm ${bootmarg}; " \
+		"fi; " \
+	"\0" \
+	"mmcboot=setenv -f _bootpart ${bootparta}; " \
+		"run importbootenv; " \
+		"setenv -f bootmarg ${loadaddr}; " \
+		"if test -n ${fitconfig}; then " \
+			"setenv -f bootmarg ${loadaddr}#${fitconfig}; " \
+		"fi; " \
+		"run mmctryboot; " \
+		"if test -n ${bootpart} && test ${bootpart} != none; then " \
+			"setenv -f _bootpart ${bootpart}; " \
+		"fi; " \
+		"echo Booting ${bootfile} from mmc ${_bootpart} ...; " \
+		"part uuid mmc ${_bootpart} bootuuid; " \
+		"setenv bootargs ${bootargs_secureboot} console=${console} " \
+			"bootenv=PARTUUID=${bootenvuuid} " \
+			"root=PARTUUID=${bootuuid} rootwait rw " \
+			"${bootargs_append}; " \
+		"ext4load mmc ${_bootpart} ${loadaddr} ${bootfile} && " \
+			"bootm ${bootmarg}; " \
+		"if test ${_bootpart} = ${bootparta}; then " \
+			"setenv -f _bootpart ${bootpartb}; " \
 		"else " \
-			"${get_cmd} ${loadaddr} ${image}; " \
-			"if test ${boot_fit} = yes || test ${boot_fit} = try; then " \
-				"bootm ${loadaddr}; " \
-			"else " \
-				"if ${get_cmd} ${fdt_addr_r} ${fdtfile}; then " \
-					"run boot_os; " \
-				"else " \
-					"echo WARN: Cannot load the DT; " \
-				"fi; " \
-			"fi;" \
-		"fi;\0" \
-	"bsp_bootcmd=echo Running BSP bootcmd ...; " \
-		"mmc dev ${mmcdev}; if mmc rescan; then " \
-		   "if run loadbootscript; then " \
-			   "run bootscript; " \
-		   "else " \
-			   "if test ${sec_boot} = yes; then " \
-				   "if run loadcntr; then " \
-					   "run mmcboot; " \
-				   "else run netboot; " \
-				   "fi; " \
-			    "else " \
-				   "if run loadimage; then " \
-					   "run mmcboot; " \
-				   "else run netboot; " \
-				   "fi; " \
-				"fi; " \
-		   "fi; " \
-	   "fi;"
+			"setenv -f _bootpart ${bootparta}; " \
+		"fi; " \
+		"echo Failover boot ${bootfile} from mmc ${_bootpart} ...; " \
+		"part uuid mmc ${_bootpart} bootuuid; " \
+		"setenv bootargs ${bootargs_secureboot} console=${console} " \
+			"bootenv=PARTUUID=${bootenvuuid} " \
+			"root=PARTUUID=${bootuuid} rootwait rw " \
+			"systemd.unit=rescue.target " \
+			"${bootargs_append}; " \
+		"ext4load mmc ${_bootpart} ${loadaddr} ${bootfile} && " \
+			"bootm ${bootmarg}; " \
+	"\0"
+
 
 /* Link Definitions */
 
@@ -155,20 +122,12 @@
 #define CFG_SYS_INIT_RAM_SIZE        0x200000
 
 #define CFG_SYS_SDRAM_BASE           0x80000000
-#define PHYS_SDRAM                      0x80000000
-#define PHYS_SDRAM_SIZE			0x80000000 /* 2GB DDR */
+#define PHYS_SDRAM                   0x80000000
+#define PHYS_SDRAM_SIZE			     0x80000000 /* 2GB DDR */
 
 #define CFG_SYS_FSL_USDHC_NUM	2
 
 /* Using ULP WDOG for reset */
 #define WDOG_BASE_ADDR          WDG3_BASE_ADDR
-
-#if defined(CONFIG_CMD_NET)
-#define PHY_ANEG_TIMEOUT 20000
-#endif
-
-#ifdef CONFIG_IMX_MATTER_TRUSTY
-#define NS_ARCH_ARM64 1
-#endif
 
 #endif
